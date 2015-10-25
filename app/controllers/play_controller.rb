384175ae301bda61a76b9cdb6953962e3df486b0
Jan-Hettich <janhettich@earthlink.net>
2015-10-25 04:20:44 -0700

    receive api call working

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,19 +1,19 @@
 # play controller
-# require 'open-uri'
-# require 'json'
+require 'open-uri'
+require 'json'
+require 'erb'
+include ERB::Util
 
 class PlayController < ApplicationController
 
 	@@xpub_key = 'xpub6CmLL1Yyc3t8FXNxfmkenahv36EB4mbuViBu4mVo5MAx5DvbvJzZuNvZyMTiTe8ttfmGmit94yL8ELsYYD6y4A857u9tGmjyobhmzCcoPWg'
-	@@callback_url = 'http://palazzo.eu-gb.mybluemix.net/callback'
+	@@callback_url = 'http://palazzo.eu-gb.mybluemix.net/blockchain'
 	@@api_key = '2aba459d-420a-497c-bfbc-c99fb58dd40e'
 
+
 	def entry
-		# uri = 'https://api.blockchain.info/v2/receive?' + @@xpub_key + '&callback=' + @@callback_url + '&key=' + @@api_key
-		# response = open(uri)
-		# raise "no receive address returned from blockchain.info" if response.status != 200
-		# json_response = response.body
-		# hash = JSON.parse(json_response)
-		@game_address = 'foo' #hash['address']
+		uri = 'https://api.blockchain.info/v2/receive?xpub=' + @@xpub_key + '&callback=' + url_encode(@@callback_url) + '&key=' + @@api_key
+		response = open(uri).read
+		@game_address = JSON.parse(response)['address']
 	end
 end
