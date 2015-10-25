a76bf9816caba1699f26b84fb1cc2759d9c4c5fb
Jan-Hettich <janhettich@earthlink.net>
2015-10-25 05:12:00 -0700

    implemented callback

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -7,13 +7,19 @@ include ERB::Util
 class PlayController < ApplicationController
 
 	@@xpub_key = 'xpub6CmLL1Yyc3t8FXNxfmkenahv36EB4mbuViBu4mVo5MAx5DvbvJzZuNvZyMTiTe8ttfmGmit94yL8ELsYYD6y4A857u9tGmjyobhmzCcoPWg'
-	@@callback_url = 'http://palazzo.eu-gb.mybluemix.net/blockchain'
+	@@callback_url = 'http://palazzo.eu-gb.mybluemix.net/play/callback'
 	@@api_key = '2aba459d-420a-497c-bfbc-c99fb58dd40e'
-
 
 	def entry
 		uri = 'https://api.blockchain.info/v2/receive?xpub=' + @@xpub_key + '&callback=' + url_encode(@@callback_url) + '&key=' + @@api_key
+		puts uri
 		response = open(uri).read
 		@game_address = JSON.parse(response)['address']
 	end
+
+	def callback
+		p params
+		render text: '*ok*' and return
+	end
 end
+
