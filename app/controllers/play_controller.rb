d15ca238d0931cf682e20265b112857d83b65f85
Jan-Hettich <janhettich@earthlink.net>
2015-10-25 02:36:23 -0700

    comment out failing code

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,6 +1,6 @@
 # play controller
-require 'open-uri'
-require 'json'
+# require 'open-uri'
+# require 'json'
 
 class PlayController < ApplicationController
 
@@ -9,11 +9,11 @@ class PlayController < ApplicationController
 	@@api_key = '2aba459d-420a-497c-bfbc-c99fb58dd40e'
 
 	def entry
-		uri = 'https://api.blockchain.info/v2/receive?' + @@xpub_key + '&callback=' + @@callback_url + '&key=' + @@api_key
-		response = open(uri)
-		raise "no receive address returned from blockchain.info" if response.status != 200
-		json_response = response.body
-		hash = JSON.parse(json_response)
-		@game_address = hash['address']
+		# uri = 'https://api.blockchain.info/v2/receive?' + @@xpub_key + '&callback=' + @@callback_url + '&key=' + @@api_key
+		# response = open(uri)
+		# raise "no receive address returned from blockchain.info" if response.status != 200
+		# json_response = response.body
+		# hash = JSON.parse(json_response)
+		@game_address = 'foo' #hash['address']
 	end
 end
