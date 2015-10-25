06de13ba5ee267b535f2f865bf88a5e4ded1fafb
Jan-Hettich <janhettich@earthlink.net>
2015-10-25 02:23:56 -0700

    get receive address from blockchain

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,8 +1,19 @@
 # play controller
+require 'open-uri'
+require 'json'
 
 class PlayController < ApplicationController
 
+	@@xpub_key = 'xpub6CmLL1Yyc3t8FXNxfmkenahv36EB4mbuViBu4mVo5MAx5DvbvJzZuNvZyMTiTe8ttfmGmit94yL8ELsYYD6y4A857u9tGmjyobhmzCcoPWg'
+	@@callback_url = 'http://palazzo.eu-gb.mybluemix.net/callback'
+	@@api_key = '2aba459d-420a-497c-bfbc-c99fb58dd40e'
+
 	def entry
-		@game_address = "xxxxxxx"
+		uri = 'https://blockchain.info/v2/receive?' + @@xpub_key + '&callback=' + @@callback_url + '&key=' + @@api_key
+		response = open(uri)
+		raise "no receive address returned from blockchain.info" if response.status != 200
+		json_response = response.body
+		hash = JSON.parse(json_response)
+		@game_address = hash['address']
 	end
 end
